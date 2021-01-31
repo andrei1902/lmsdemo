b0b73c592691ba4554efa4dbaf5ee9fb1505cf1d
andrei19021 <andrei.ionita.1902@gmail.com>
2021-01-31 13:20:21 +0200

    feat: Adds Courses, Course and NotFound in pages

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return <div>The page was not found</div>
+}
---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+
+export { NotFound } from './NotFound.js';
+export { Courses } from './Courses.js';
+export { Course } from './Course.js';
