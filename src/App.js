7e784336aa72ba5b5c7d62fb587570676a0e0151
andrei19021 <andrei.ionita.1902@gmail.com>
2021-01-31 13:21:04 +0200

    feat: Adds routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React, { Suspense } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      LMS 365 Demo
-    </div>
-  );
+import { Header } from './components/layout/'
+import { Preloader } from './components/reusable/'
+import { Courses, Course, NotFound } from './components/pages/'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    // check locale setup on localStorage
+  }
+
+  componentDidMount () {
+    // Load texts based on locale
+    // Assert loginStatus
+  }
+
+  render () {
+    const { history } = this.props
+    // Conditional rendering based on login status
+    return (
+      <div className="App">
+        <Suspense fallback={<Preloader />}>
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Courses} history={history} />
+            <Route exact path="/courses" component={Courses} history={history} />
+            <Route exact path="/courses/:id" component={Course} history={history} />
+            <Route component={NotFound} history={history} />
+          </Switch>
+        </Suspense>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App)
