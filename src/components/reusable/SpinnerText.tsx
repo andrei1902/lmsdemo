930642cebf248b359dafaea0d29d1f9527da1eec
andrei19021 <andrei.ionita.1902@gmail.com>
2021-02-13 12:55:54 +0200

    Pimped up the spinner

--- src/components/reusable/SpinnerText.tsx
+++ src/components/reusable/SpinnerText.tsx
@@ -1,15 +1,35 @@
 import React, { Fragment } from 'react'
-import useLayout from '../../hooks/useLayout'
+import { makeStyles } from '@material-ui/core/styles';
+import useLayout from '../../hooks/useLayout';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(1),
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    }
+  }
+}));
 
 export const SpinnerText = (): JSX.Element|null => {
-  const [layout] = useLayout()
+  const [layout] = useLayout();
+  const classes = useStyles();
 
   if (layout.spinners.length) {
     return (
       <Fragment>
-        {layout.spinners.map((txt: any): any => {
-          return (<p>{layout.texts.templates.dataTypesLoading.replace('__dateTypes__', txt)}</p>)
-        })}
+        <div className={classes.root}>
+          {layout.spinners.map((txt: any): any => {
+            return (
+              <Typography variant="body2" component="p" align="center">
+                {layout.texts.templates.dataTypesLoading.replace('__dateTypes__', txt)}
+              </Typography>
+            )
+          })}
+          <CircularProgress />
+        </div>
       </Fragment>
     )
   }
